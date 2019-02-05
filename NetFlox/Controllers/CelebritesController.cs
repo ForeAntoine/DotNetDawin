11748ed98050236a75a43ed457b5b1e27f24b661
ForeAntoine <fore.antoine@gmail.com>
2019-02-05 22:22:41 +0100

    init commit
    initial commit

---
+++ NetFlox/Controllers/CelebritesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NetFlox.DAL;
+
+namespace NetFlox.Controllers
+{
+    public class CelebritesController : Controller
+    {
+        private readonly NetFloxEntities _context;
+
+        public CelebritesController(NetFloxEntities context)
+        {
+            _context = context;
+        }
+
+        // GET: Celebrites
+        public async Task<IActionResult> Index()
+        {
+            var celebrites = await _context.Celebrites
+                .OrderBy(c => c.Nom)
+                .ToListAsync();
+            return View(celebrites);
+        }
+
+        // GET: Celebrites/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var celebrite = await _context.Celebrites
+                .Include(c => c.RoleCelebriteFilms)
+                .ThenInclude(rcf => rcf.Role)
+                .Include(c => c.RoleCelebriteFilms)
+                .ThenInclude(rcf => rcf.Film)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (celebrite == null)
+            {
+                return NotFound();
+            }
+
+            return View(celebrite);
+        }
+
+        // GET: Celebrites/Search
+        public async Task<IActionResult> Search(string celebrite)
+        {
+            var films = await _context.Celebrites
+                .Where(c => c.Nom.ToLower().Contains(celebrite.ToUpper()))
+                .OrderByDescending(c => c.Nom)
+                .ToListAsync();
+            return View(films);
+        }
+    }
+}
